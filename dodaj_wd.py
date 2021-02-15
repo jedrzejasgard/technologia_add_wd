5ba9d2449e951b0fff4fea970c2eec04c354de93
jedrzej <j.pawlewski@asgard.gifts>
2021-02-15 11:22:55 +0100

    Dodanie dict z nazwami-wartosciami Wartosci dowolnych

--- dodaj_wd.py
+++ dodaj_wd.py
@@ -12,6 +12,15 @@ vendoApi.logInApi(config.get('vendo','logInApi_user'),config.get('vendo','logInA
 vendoApi.loginUser(config.get('vendo','loginUser_user'),config.get('vendo','loginUser_pass'))
 
 df = pd.read_excel(f'UV-wartosci.xls',encoding='utf-8')
+with open('Nazwy_wd_tech.xls','rb') as tabela_nazw_wd:
+    df_WD_technologii = pd.read_excel(tabela_nazw_wd)
+    df_WD_technologii = df_WD_technologii.fillna('wolna')
+# tworzenie dictionary z nazwami WD
+wd_dict = dict()
+for index, row in df_WD_technologii.iterrows():
+    if row['Nazwa'] == 'wolna' or row['Zmienna'] == 'wolna':
+        continue
+    wd_dict[row['Nazwa']] = row['Zmienna']
 
 for index, row in df.iterrows():
     idTechnologii = row['ID']
